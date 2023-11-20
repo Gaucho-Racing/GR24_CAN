e0fa703ca105e621d0d1f79f107e4a336fc63c3c
Nikunj Parasar <nikunj.parasar@gmail.com>
2023-11-20 01:57:18 -0800

    Sensor Nodes I think??

--- Nodes.h
+++ Nodes.h
@@ -18,17 +18,6 @@
 
 
 
-struct VDM {
-
-};
-
-struct Sensors {
-
-};
-
-struct Energy_Meter {
-
-};
 
 struct Inverter {
     byte data[5][8]={{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
@@ -51,7 +40,6 @@ struct Inverter {
     ----------------------------------------------------------------------------------------------------------------------------------
     0x2416 |  Throttle      |	Brake	    |   Digital IO	|  Drive Enable	|     Flags     |	Flags	    | RESERVED	    |CAN Version
     -----------------------------------------------------------------------------------------------------------------------------------------
-
     */
 
 
@@ -89,6 +77,7 @@ struct Inverter {
         Can1.write(msg);
     }
     unsigned long getID() {return ID;}
+
     long getERPM() {return(((long)data[0][0] << 24) + ((long)data[0][1] << 16) + ((long)data[0][2] << 8) + data[0][3]);} //rpm/pole pairs
     float getDuty() {return((((long)data[0][4] << 8) + data[0][5])/10);} //i think [0,100]. Related to top speed
     int getVoltIn() {return(((long)data[0][6] << 8) + data[0][7]);}
@@ -123,6 +112,7 @@ struct Inverter {
     bool getPowerLimit() {return ((data[4][5] & 0x20) == 0x20);}//    
 
 
+
     void setCurrent(float in) {send(0x1A, (long)(in*10), 2);}//            ^
     void setBrakeCurrent(float in) {send(0x1B, (long)(in*10), 2);}//       ^
     void setERPM(long in) {send(0x1C, (long)in, 4);}//                     ^
@@ -135,10 +125,12 @@ struct Inverter {
     void setMaxDCBrakeCurrent(float in) {send(0x23, (long)(in*10), 2);}//  ^
     void setDriveEnable(byte in) {send(0x24, (long)in, 1);} //Enable/disable motor
     unsigned long getAge(){return(millis() - receiveTime);} //time since last data packet
+};
 
-
+struct VDM {
 
 };
+
 
 struct BCM {
 
@@ -149,6 +141,76 @@ struct ACU {
 };
 
 struct Dash {
+
+};
+
+
+struct Sensors {
+    // REFERENCE GR24 CAN BUS DATA SHEET
+   
+    unsigned long ID = 0;
+    FlexCAN_T4<CAN_DATA_BUS, RX_SIZE_256, TX_SIZE_16> Can1;
+    CAN_message_t msg;
+    unsigned long receiveTime = 0;
+
+    Sensors(unsigned long id, FlexCAN_T4<CAN_DATA_BUS, RX_SIZE_256, TX_SIZE_16> &can) : ID(id){
+        can = Can1;
+    }
+
+    void receive(unsigned long id, byte buf[]){
+        // idfk 
+        // ... polymorphism using c++ or some shit
+        // or just not
+    }
+
+     struct Wheel_FR {
+         byte data[5][8] = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; 
+    };
+    struct Wheel_FL {
+        byte data[5][8] = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; 
+    };
+    
+    struct Wheel_RR {
+        byte data[5][8] = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; 
+    };
+
+    struct Wheel_RL {
+        byte data[5][8] = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; 
+    };
+    
+    struct Central_IMU {
+        byte data[3][8] = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  //Accel
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, //Gyro
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; //Mag
+
+    };
+
+    struct GPS {
+        byte data[4][8] = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  //Latitude
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, //Longitude
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, //Other
+                    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; //Other 2
+    };
+
+};
+
+struct Energy_Meter {
 
 };
 
